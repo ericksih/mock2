7f504387bb7b4e136daaa638540e74b1bcaa1ce6
ericksih <erick.saputra08@gmail.com>
2021-03-15 22:26:37 +0700

    simulasiujian

---
+++ ERICK/test.js
@@ -0,0 +1,20 @@
+let tombol1 = document.getElementById ("result")
+
+
+function type1 (){
+    
+    var result = ""
+
+    for(var i = 0; i< 6; i++){
+      for(var l = 0;l<3;l++){
+        result += "*#"
+      }
+      result += "\n"
+    }
+     return result   
+
+}
+
+
+
+
